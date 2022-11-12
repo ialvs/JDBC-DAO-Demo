b882508fecd537cb94bae4bf9cdfa1a454f559de
Ian Alves <ian.santos.alves@hotmail.com>
2022-11-12 14:40:37 -0300

    Implementação findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDAO;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -13,7 +16,15 @@ public class Program {
 		Seller seller = sellerDao.findById(1);
 		
 		System.out.println(seller);
-
+		
+		System.out.println("=== TEST 1: seller findByDepartment ===");
+		Department department = new Department(2,null);
+		List<Seller> sellers = sellerDao.findByDepartment(department);
+		
+		for (Seller sellersItem : sellers) {
+			System.out.println(sellersItem);
+		}
 	}
 
 }
+
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -13,65 +16,59 @@ import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDAO {
-	
+
 	private Connection conn;
-	
+
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	@Override
 	public void insert(Seller obj) {
-		
-		
+
 	}
 
 	@Override
 	public void update(Seller obj) {
-		
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		
-		
+
 	}
 
 	@Override
 	public Seller findById(Integer id) {
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
 			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentID = department.Id "
-					+ "WHERE seller.Id = ?"
-					);
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentID = department.Id " + "WHERE seller.Id = ?");
 			st.setInt(1, id);
 			rs = st.executeQuery();
-			
+
 			if (rs.next()) {
 				Department dep = instantiateDepartment(rs);
-				
-				Seller seller = instatiateSeller(rs,dep);
+
+				Seller seller = instatiateSeller(rs, dep);
 				return seller;
-				
+
 			}
-			
+
 			return null;
-		
-		}catch(SQLException e) {
+
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}finally {
-			
+		} finally {
+
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
-			
+
 		}
-		
+
 	}
 
 	private Seller instatiateSeller(ResultSet rs, Department dep) throws SQLException {
@@ -82,7 +79,7 @@ public class SellerDaoJDBC implements SellerDAO {
 		seller.setBaseSalary(rs.getDouble("BaseSalary"));
 		seller.setBirthDate(rs.getDate("BirthDate"));
 		seller.setDepartment(dep);
-		
+
 		return seller;
 	}
 
@@ -95,8 +92,50 @@ public class SellerDaoJDBC implements SellerDAO {
 
 	@Override
 	public List<Seller> findAll() {
-		
+
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentID = department.Id " + "WHERE DepartmentId = ? " + "ORDER BY Name");
+			st.setInt(1, department.getId());
+
+			rs = st.executeQuery();
+
+			List<Seller> sellers = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+
+			while (rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller seller = instatiateSeller(rs, dep);
+				sellers.add(seller);
+
+			}
+
+			return sellers;
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+
+		}
+
 	}
 
 }
