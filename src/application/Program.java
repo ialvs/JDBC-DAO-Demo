668901ff7f50821becb452cf40f79ad0982799c6
Ian Alves <ian.santos.alves@hotmail.com>
2022-11-12 14:21:01 -0300

    Reusando instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,8 @@ public class Program {
 	public static void main(String[] args) {
 		SellerDAO sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = sellerDao.findById(3);
+		System.out.println("=== TEST 1: seller findById ===");
+		Seller seller = sellerDao.findById(1);
 		
 		System.out.println(seller);
 
